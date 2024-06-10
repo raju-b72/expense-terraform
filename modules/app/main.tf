f9890689be978b967521c983e88b04f51c3a204a
rajendra <rajendrammarboyana@gmail.com>
2024-06-10 14:43:14 +0100

    newrelic setu

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -12,19 +12,19 @@ resource "aws_instance" "instance" {
 }
 #g
 resource "null_resource" "ansible" {
- provisioner "remote-exec" {
-  connection {
-    type     = "ssh"
-    user     = var.ssh_user
-    password = var.ssh_pass
-    host     = aws_instance.instance.public_ip
-  }
+  provisioner "remote-exec" {
+     connection {
+       type     = "ssh"
+       user     = var.ssh_user
+       password = var.ssh_pass
+       host     = aws_instance.instance.public_ip
+     }
 
-  inline = [
-    "sudo pip3.11 install ansible ",
-    "ansible-pull -i localhost, -U https://github.com/raju-b72/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component}"
+     inline = [
+       "sudo pip3.11 install ansible ",
+       "ansible-pull -i localhost, -U https://github.com/raju-b72/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component}"
 
-  ]
+     ]
   }
 }
 resource "aws_route53_record" "server" {
