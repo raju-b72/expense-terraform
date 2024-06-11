e2d9036a3043c92453374c15b4351a82da0d531e
rajendra <rajendrammarboyana@gmail.com>
2024-06-11 07:55:10 +0100

    vault vars add to pipeline

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -15,8 +15,8 @@ resource "null_resource" "ansible" {
   provisioner "remote-exec" {
      connection {
        type     = "ssh"
-       user     =  jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
-       password =  jsondecode(data.vault_generic_secret.ssh.data_json).ansible_pass
+       user     =  jsondecode(data.vault_generic_secret.ssh.data_json).user
+       password =  jsondecode(data.vault_generic_secret.ssh.data_json).pass
        host     = aws_instance.instance.public_ip
      }
 
