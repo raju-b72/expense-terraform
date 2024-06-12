16ab2dabb4c237da194e5409e63996d52c504f3d
rajendra <rajendrammarboyana@gmail.com>
2024-06-12 10:21:01 +0100

    install logstash because undo did git commit before

--- main.tf
+++ main.tf
@@ -1,30 +1,40 @@
-module "frontend" {
-  depends_on = [module.backend]
-  source = "./modules/app"
-  instance_type= var.instance_type
-  component = "frontend"
-  env = var.env
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-}
+# module "frontend" {
+#   depends_on = [module.backend]
+#   source = "./modules/app"
+#   instance_type= var.instance_type
+#   component = "frontend"
+#   env = var.env
+#   zone_id = var.zone_id
+#   vault_token = var.vault_token
+# }
+#
+# module "backend" {
+#   depends_on = [module.mysql]
+#   source = "./modules/app"
+#   instance_type= var.instance_type
+#   component = "backend"
+#   env = var.env
+#   zone_id = var.zone_id
+#   vault_token = var.vault_token
+#
+# }
+#
+#
+# module "mysql" {
+#   source = "./modules/app"
+#   instance_type= var.instance_type
+#   component = "mysql"
+#   env = var.env
+#   zone_id = var.zone_id
+#   vault_token = var.vault_token
+# }
+#
 
-module "backend" {
-  depends_on = [module.mysql]
-  source = "./modules/app"
-  instance_type= var.instance_type
-  component = "backend"
-  env = var.env
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-
-}
 
 
-module "mysql" {
-  source = "./modules/app"
-  instance_type= var.instance_type
-  component = "mysql"
-  env = var.env
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-}
+# module "vpc" {
+#   source = "./modules/vpc"
+#   env = var.env
+#   vpc_cidr_block = var.vpc_cidr_block
+#   default_vpc_id = var.default_vpc_id
+#   default_vpc_cidr = var.default_vpc_cidr
