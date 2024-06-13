e7e40ea157976ae5616bf98b11d12089156c12d5
rajendra <rajendrammarboyana@gmail.com>
2024-06-13 17:15:53 +0100

    ami

--- modules/app-asg/data.tf
+++ modules/app-asg/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "golden-ami-*"
-  owners           = ["self"]
+  name_regex       = "RHEL-9-DevOps-Practice"
+  owners           = ["973714476881"]
 
 }
 
